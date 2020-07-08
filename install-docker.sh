96d0d1a6d35074b4d8b611e70f5a58695558d4b4
RajaHamza99 <65599529+RajaHamza99@users.noreply.github.com>
2020-07-08 12:15:09 +0100

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -4,6 +4,6 @@ curl https://get.docker.com | sudo bash
 echo "add user to docker group"
 sudo usermod -aG docker $(whoami)
 echo "install docker-compose"
-sudo curl -L 'https://github.com/docker/compose/releases/download/\${version}/docker-compose-\$(uname -s)-\$(uname -m)' -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 echo "making docker-compose executable"
 sudo chmod +x /usr/local/bin/docker-compose
