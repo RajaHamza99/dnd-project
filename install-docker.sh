f078abd28ce4c21b97a5230f63aafc2321e755e0
RajaHamza99 <65599529+RajaHamza99@users.noreply.github.com>
2020-07-08 12:07:49 +0100

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -1,4 +1,7 @@
 #! /bin/bash
+echo "installing docker"
 curl https://get.docker.com | sudo bash
+echo "install docker-compose"
 sudo curl -L 'https://github.com/docker/compose/releases/download/\${version}/docker-compose-\$(uname -s)-\$(uname -m)' -o /usr/local/bin/docker-compose
+echo "making docker-compose executable"
 sudo chmod +x /usr/local/bin/docker-compose
