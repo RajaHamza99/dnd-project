acf403deb7ae6c60c196f553d4c582bd7c9be7be
RajaHamza99 <65599529+RajaHamza99@users.noreply.github.com>
2020-07-10 20:50:12 +0100

    Update docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -3,6 +3,6 @@ docker-compose build
 docker-compose push
 ssh jenkins@dnd-manager << EOF
 cd /home/rajahamza104/dnd-project
-git checkout development2
+git checkout development
 env SECRET_KEY=${SECRET_KEY} env DB_USER=${DB_USER} env DB_PASS=${DB_PASS} docker stack deploy --compose-file docker-compose.yaml dnd
 EOF
