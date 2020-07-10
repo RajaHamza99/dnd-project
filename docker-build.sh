697ef3ffaebb01d3abf6cc8c03048354204b69fb
RajaHamza99 <65599529+RajaHamza99@users.noreply.github.com>
2020-07-10 13:58:37 +0100

    Update docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -1,9 +1,8 @@
 #! /bin/bash
+docker-compose build
+docker-compose push
 ssh jenkins@dnd-manager
-pwd
 export SECRET_KEY=apogjmnaiopdgn
 export DB_USER=hamza
 export DB_PASS=hamza2
-docker-compose build
-docker-compose push
 docker stack deploy --compose-file docker-compose.yaml dnd
