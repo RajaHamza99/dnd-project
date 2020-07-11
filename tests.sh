58d3839ceaea74ef97ccd942f44cdf396af430cc
hamza <hraja@academytrainee.com>
2020-07-11 19:18:47 +0000

    checking directory

--- tests.sh
+++ tests.sh
@@ -2,7 +2,13 @@
 
 . /home/rajahamza104/dnd-project/venv/bin/activate
 
+echo pwd
 cd /home/jenkins/.jenkins/workspace/dnd-app/service_one
+echo pwd
+ls
 pytest ./service_one --cov ./service_one/
+echo pwd
+ls
+
 pytest ./service_two --cov ./service_two/
 pytest ./service_three --cov ./service_three/
