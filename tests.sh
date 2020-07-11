ee4f48989705ddbb13b7b242f6587e38e4122d0f
hamza <hraja@academytrainee.com>
2020-07-11 19:16:59 +0000

    testing script changed directory

--- tests.sh
+++ tests.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
 . /home/rajahamza104/dnd-project/venv/bin/activate
+
+cd /home/jenkins/.jenkins/workspace/dnd-app/service_one
 pytest ./service_one --cov ./service_one/
 pytest ./service_two --cov ./service_two/
 pytest ./service_three --cov ./service_three/
