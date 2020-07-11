e2b09d5d1c305a646d2e08d023bf66294f939f91
hamza <hraja@academytrainee.com>
2020-07-11 19:31:31 +0000

    testing services 1-3 with jenkins

--- tests.sh
+++ tests.sh
@@ -9,7 +9,7 @@ cd /home/jenkins/.jenkins/workspace/dnd-app/service_one
 pytest --cov ./application
 
 cd /home/jenkins/.jenkins/workspace/dnd-app/service_two
-pytest ./service_two --cov ./service_two/
+pytest --cov ./application
 
 cd /home/jenkins/.jenkins/workspace/dnd-app/service_three
-pytest ./service_three --cov ./service_three/
+pytest --cov ./application
