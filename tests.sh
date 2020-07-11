b23b6ff437f323d8c112ff4ecc62d32806791a72
hamza <hraja@academytrainee.com>
2020-07-11 19:12:57 +0000

    testing script venv

--- tests.sh
+++ tests.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-. /home/rajahamza104/dnd-project
+. /home/rajahamza104/dnd-project/venv/bin/activate
 pytest ./service_one --cov ./service_one/
 pytest ./service_two --cov ./service_two/
 pytest ./service_three --cov ./service_three/
